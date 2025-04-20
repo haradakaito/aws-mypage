dd3671ac238f2ac1a5b899f9a42cdf95570e5f53
muumin <harada.kaito.20@shizuoka.ac.jp>
2025-04-20 16:46:13 +0900

    [add] strictモードの解除，useEffectの削除

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,21 +1,8 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import HomePageJP from "./pages/HomePageJP";
 import HomePageEN from "./pages/HomePageEN";
-import { useEffect } from "react";
 
 const App = () => {
-  useEffect(() => {
-    if (sessionStorage.getItem("hasVisited") === null) {
-      sessionStorage.setItem("hasVisited", "true");
-      const baseUrl = process.env.REACT_APP_API_BASE_URL;
-
-      fetch(`${baseUrl}/view`, { method: "POST" })
-        .catch((err) => {
-          console.error("Failed to update view count:", err);
-        });
-    }
-  }, []);
-
   return (
     <BrowserRouter>
       <Routes>
